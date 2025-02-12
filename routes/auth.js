0a8572fa9ab91b5499b1b7b683ebec07fbd0676e
Michael Mascari <michael@mascari.me>
2025-02-12 18:34:26 +0000

    fixed: misnamed require

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const sqlite3 = require('sqlite3').verbose();
 
 const router = express.Router();
