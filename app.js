f2096d8fc794debe90f0c54fcd556b4105fed50f
mmascari99 <michael@mascari.me>
2025-02-12 18:06:33 -0600

    Attempt 6. Might try making test enviornment...

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.post('/login', passport.authenticate('local', {
 }));
 
 app.get('/dashboard', (req, res) => {
-  res.render('dashboard', { username: req.user.id });
+  res.render('dashboard', { username: req.user });
 });
 
 // Resume route.
